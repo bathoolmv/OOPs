05de239c80469fcddc3eaf95eb7b6a829cc550c0
bathoolmv <155854565+bathoolmv@users.noreply.github.com>
2024-02-29 15:45:37 +0530

    java program using constructor

---
+++ Constructor1.java
@@ -0,0 +1,22 @@
+class Employee
+{
+ 	int eno;
+ 	String eName;
+ 	Employee()
+{
+	eno=100;
+	eName="Adarsh";
+}
+	void show()
+	{
+	System.out.println("\nEmployee No:"+eno+"\nEmployee Name:"+eName);
+	}
+}
+class Constructor1
+{
+	public static void main(String args[])
+	{
+		Employee obj1=new Employee();
+		obj1.show();
+	}
+}
